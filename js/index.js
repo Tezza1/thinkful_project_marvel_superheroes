8afb27a0cd86fb6f064f509342827a8ffda795d7
Terry <tshenker@gmail.com>
2018-07-30 07:43:06 +0000

    added error checking

--- js/index.js
+++ js/index.js
@@ -4,10 +4,10 @@ let superhero_character = "";
 function superHeroSearch() {
   $(".js-superhero-search").click(
     function(){
-      superhero_character = $('.superhero-search').val();
-      getComicData();
-      getMovieData();
-      findYouTubeVideos()
+        superhero_character = $('.superhero-search').val();
+        getComicData();
+        getMovieData();
+        findYouTubeVideos()
   });  
 }
 
@@ -25,12 +25,10 @@ function getComicData(){
         dataType: 'json',
         type: 'GET',
         success: function() {
-            // console.log(search_results);
-            // console.log(search_results.responseJSON.data.results);
             renderComicData(search_results);
         },
         error: function() {
-            console.log("An error occurred");
+           console.log('An error occurred');
         }
     };
     let search_results = $.ajax(settings);
@@ -39,23 +37,32 @@ function getComicData(){
 // render comic data to the page
 function renderComicData(search_results) {
     const COMIC_PATH = search_results.responseJSON.data.results;
-    $('.character-name').html(`${COMIC_PATH[0].name}`);
-    $('.character-description').html(`${COMIC_PATH[0].description}`);
-    $('.character-pic').html(`<img src="${COMIC_PATH[0].thumbnail.path}.jpg" alt="" class="character-pic"/>`);
+    if (COMIC_PATH.length) {
+        $('.character-name').html(`${COMIC_PATH[0].name}`);
+        $('.character-description').html(`${COMIC_PATH[0].description}`);
+        $('.character-pic').html(`<img src="${COMIC_PATH[0].thumbnail.path}.jpg" alt="" class="character-pic"/>`);
+        
+        // show links in a list
+        let character_links = "";
+        for (let i = 0; i < COMIC_PATH[0].urls.length; i++) {
+            character_links += `<div class="ext-button"><a href='${COMIC_PATH[0].urls[i].url}' target="_blank" class="external-link">${COMIC_PATH[0].urls[i].type} <i class="fas fa-external-link-alt"></i></a></div>`
+        }
+        $('.character-links').html(character_links);
     
-    // show links in a list
-    let character_links = "";
-    for (let i = 0; i < COMIC_PATH[0].urls.length; i++) {
-        character_links += `<div class="ext-button"><a href='${COMIC_PATH[0].urls[i].url}' target="_blank" class="external-link">${COMIC_PATH[0].urls[i].type} <i class="fas fa-external-link-alt"></i></a></div>`
+        // show stories in a list
+        let comic_stories = "";
+        for (let i = 0; i < COMIC_PATH[0].stories.items.length; i++){
+            comic_stories += `<div><i class="fas fa-square"></i> ${COMIC_PATH[0].series.items[i].name}</div>`
+        }
+        $('.display-comic-results').html(comic_stories);
     }
-    $('.character-links').html(character_links);
-    
-    // show stories in a list
-    let comic_stories = "";
-    for (let i = 0; i < COMIC_PATH[0].stories.items.length; i++){
-        comic_stories += `<div><i class="fas fa-square"></i> ${COMIC_PATH[0].series.items[i].name}</div>`
+    else {
+        $('.character-name').html(`<p></p>`);
+        $('.character-description').html(`<p></p>`);
+        $('.character-pic').html(`<p></p>`);
+        $('.character-links').html('<p></p>');
+        $('.display-comic-results').html('<p><i class="fas fa-exclamation-triangle"></i> No data to display</p>');
     }
-    $('.display-comic-results').html(comic_stories);
 }
 
 // AJAX call for movie data
@@ -69,23 +76,21 @@ function getMovieData() {
         data: {
             api_key: API_KEY,
             query: superhero_character,
-            with_genres: 878
         },
         dataType: 'json',
         type: 'GET',
         success: function() {
-            // console.log(search_results);
-            // console.log(search_results.responseJSON.results);
             renderMovieData(search_results.responseJSON.results);
         },
         error: function() {
-            console.log("An error occurred");
+            console.log('An error occurred');
         }
     };
 
     let search_results = $.ajax(settings);
 }
 
+// render movie data to the page
 function renderMovieData(search_results) {
     let movieData ="";
     let display_data = false;
@@ -111,11 +116,16 @@ function renderMovieData(search_results) {
         }
         display_data = false;
     }
-    $('.display-movies-results').html(movieData);
+    if (movieData === ""){
+        $('.display-movies-results').html('<p><i class="fas fa-exclamation-triangle"></i> No data to display</p>');
+    }
+    else {
+        $('.display-movies-results').html(movieData);
+    }
 }
 
+// AJAX call for movie data
 function findYouTubeVideos() {
-
     const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
     const API_KEY = "AIzaSyDEwfk3b9G0Oap6N7GHBeDHPzhnXxxr5hM";
 
@@ -134,28 +144,34 @@ function findYouTubeVideos() {
             renderYouTubeData(search_results);
         },
         error: function() {
-            console.log("An error occurred");
+            console.log('An error occurred');
         }
     };
 
     let search_results = $.ajax(settings);
 }
 
+// render movie data to the page
 function renderYouTubeData(search_results) {
     let result_arr = search_results.responseJSON.items;
-    let resultString = "";
-    result_arr.map(result => {
-        resultString += `
-            <div class="col-6">
-                <h4>${result.snippet.title}</h4>
-                <p>${result.snippet.description}</p>
-            </div>
-            <div class="col-6">
-                <iframe src="https://www.youtube.com/embed/${result.id.videoId}" allow="autoplay; encrypted-media" width="350" height="200" frameborder="0" allowFullScreen></iframe>
-            </div>
-            <div class="col-12 row-breaker"></div>`;
-    });
-    $('.display-trailer').html(resultString);
+    if(result_arr.length) {
+        let resultString = "";
+        result_arr.map(result => {
+            resultString += `
+                <div class="col-6">
+                    <h4>${result.snippet.title}</h4>
+                    <p>${result.snippet.description}</p>
+                </div>
+                <div class="col-6">
+                    <iframe src="https://www.youtube.com/embed/${result.id.videoId}" allow="autoplay; encrypted-media" width="350" height="200" frameborder="0" allowFullScreen></iframe>
+                </div>
+                <div class="col-12 row-breaker"></div>`;
+        });
+        $('.display-trailer').html(resultString);
+    }
+    else {
+        $('.display-trailer').html('<p><i class="fas fa-exclamation-triangle"></i> No data to display</p>');
+    }
 } 
 
 $(superHeroSearch)
